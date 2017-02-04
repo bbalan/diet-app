7eef3e5ec5698a090d10db8ed72964b2ac61b77c
Bogdan Balan <bogdan@bogdanbalan.com>
2017-02-03 20:36:52 -0700

    Macros display

--- src/components/Log/Day.vue
+++ src/components/Log/Day.vue
@@ -1,17 +1,96 @@
 <template>
   <div class="log__day">
     <h2>{{ dateFormatted }}</h2>
+    <span class="nutrient">Calories: {{ calories }}</span>
+    Macros: {{ fatPct }} / {{ carbsPct }} / {{ proteinPct }}
+    <!--<span class="nutrient">{{ carbs }}g carbs</span>-->
+    <!--<span class="nutrient">{{ fat }}g fat</span>-->
+    <!--<span class="nutrient">{{ protein }}g protein</span>-->
     <entry-list :entries="dataDay.entries"></entry-list>
   </div>
 </template>
 
 <script>
+import * as API from '../../api'
+import { roundTo } from '../../store/util'
+import store from '../../store'
 import EntryList from './EntryList'
 
 export default {
   props: ['dataDay', 'date'],
+  data() {
+    return {
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+      usdaNutrients: ['208', '204', '205', '203'],
+    }
+  },
   components: { EntryList },
+  created() {
+    const entries = this.dataDay.entries
+
+    entries.forEach((entry) => {
+      let nutrients
+
+      let calories
+      let nEnergy
+
+      let fat
+      let nFat
+
+      let carbs
+      let nCarbs
+
+      let protein
+      let nProtein
+
+      if (!store.state.log.entries[entry]) return
+      const entryData = store.state.log.entries[entry]
+
+      if (!entryData || !entryData.foodUUID) return
+      const food = store.state.foodCache.food[entryData.foodUUID]
+
+      switch (food.source) {
+        case API.USDA:
+          nutrients = food.dataFood.nutrients
+
+          nEnergy = nutrients.find(n => n.nutrient_id === '208')
+          calories = parseInt(nEnergy.value, 10) * (entryData.mass / 100)
+
+          nFat = nutrients.find(n => n.nutrient_id === '204')
+          fat = parseInt(nFat.value, 10) * (entryData.mass / 100)
+
+          nCarbs = nutrients.find(n => n.nutrient_id === '205')
+          carbs = parseInt(nCarbs.value, 10) * (entryData.mass / 100)
+
+          nProtein = nutrients.find(n => n.nutrient_id === '203')
+          protein = parseInt(nProtein.value, 10) * (entryData.mass / 100)
+          break
+        default: console.error('Unsupported source')
+          return
+      }
+
+      this.calories += calories
+      this.fat += fat
+      this.carbs += carbs
+      this.protein += protein
+    })
+  },
   computed: {
+    nutrientsSum() {
+      return this.fat + this.carbs + this.protein
+    },
+    fatPct() {
+      return roundTo((this.fat / this.nutrientsSum) * 100, 1)
+    },
+    carbsPct() {
+      return roundTo((this.carbs / this.nutrientsSum) * 100, 1)
+    },
+    proteinPct() {
+      return roundTo((this.protein / this.nutrientsSum) * 100, 1)
+    },
     dateFormatted() {
       const months = [
         'January',
@@ -37,3 +116,8 @@ export default {
   },
 }
 </script>
+
+<style scoped lang="stylus">
+.nutrient
+  margin-right 20px
+</style>
--- src/components/Log/EntryList.vue
+++ src/components/Log/EntryList.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
     <ul>
-      <entry-link v-for="entryUUID in entries" :entryUUID="entryUUID"></entry-link>
+      <entry-link v-for="entryUUID in filteredEntries" :entryUUID="entryUUID"></entry-link>
     </ul>
   </div>
 </template>
 
 <script>
+import store from '../../store'
 import EntryLink from './EntryLink'
 
 export default {
   name: 'EntryList',
   props: ['entries', 'date'],
   components: { EntryLink },
+  computed: {
+    // Filter only keys that match a value in store.state.log.entries
+    filteredEntries() {
+      return this.entries.filter(
+        entryUUID => store.state.log.entries[entryUUID]
+      )
+    },
+  },
 }
 </script>
