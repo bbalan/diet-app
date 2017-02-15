fde7812f28d055ab3b7e1c94bf6bab7564bc2675
Bogdan Balan <bogdan@bogdanbalan.com>
2017-02-14 22:12:17 -0700

    Better way of handling FoodFind tabs

--- src/components/FoodFind/index.vue
+++ src/components/FoodFind/index.vue
@@ -31,29 +31,31 @@ export default {
       ],
     }
   },
-  // Click the tab that matches the tab child path
   mounted() {
-    const tabIdx = this.foodPathNames.indexOf(this.$route.name)
-    const tabsList = this.$refs.tabs
-
-    if (tabsList) {
-      const el = tabsList.$el
-      const wrapper = el.querySelector('nav')
-      let tabs
-
-      setTimeout(() => {
-        tabs = wrapper.querySelectorAll('button')
-        if (tabs) {
-          tabs[tabIdx].click()
-        }
-      }, 500)
-    }
+    setTimeout(this.updateTabs, 300)
+  },
+  watch: {
+    $route() {
+      this.updateTabs()
+    },
   },
   methods: {
     nav(tabIdx) {
       if (tabIdx !== this.currentTab) {
         this.currentTab = tabIdx
         router.push({ name: this.foodPathNames[tabIdx] })
+      }
+    },
+    // Click the tab that matches the tab child path
+    updateTabs() {
+      const tabIdx = this.foodPathNames.indexOf(this.$route.name)
+      const tabsList = this.$refs.tabs
+
+      if (tabsList) {
+        const el = tabsList.$el
+        const wrapper = el.querySelector('nav')
+        const tabs = wrapper.querySelectorAll('button')
+        if (tabs) tabs[tabIdx].click()
       }
     },
   },
